2a69b60f80ca380140f2bb8d4b3e94c971e688ed
Fly-76 <58425266+Fly-76@users.noreply.github.com>
2021-01-05 09:04:50 +0100

    calendar behavior change, load data done earlier, change of visible month functionnal

--- src/Controller/ContributorController.php
+++ src/Controller/ContributorController.php
@@ -2,10 +2,18 @@
 
 namespace App\Controller;
 
+use App\Entity\Calendar;
+use App\Repository\CalendarRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
+use DateTime;
+use DateInterval;
+
+use Symfony\Component\Serializer\Normalizer\ObjectNormalizer;
+use Symfony\Component\Serializer\Serializer;
+
 
 /**
  * @IsGranted("IS_AUTHENTICATED_FULLY")
@@ -15,11 +23,42 @@ class ContributorController extends AbstractController
     /**
      * @Route("/agenda", name="app_agenda")
      */
-    public function agenda(): Response
+    public function agenda(CalendarRepository $calendarRepository): Response
     {
+        // following code should stay similare with CalendarController->monthEventsShow
+        $startDate = new DateTime();
+        $endDate = new DateTime();
+
+        $interval = new DateInterval('P5M');
+        $endDate->add($interval);
+        $endDate = $endDate->modify('last day of this month');
+
+        $calendars = $calendarRepository->findByMonthEvents($startDate, $endDate,  $this->getUser());
+
+        $data = [];
+        foreach ($calendars as $calendar){
+            $event = [];
+            $event['date'] = $calendar->getDate()->format('Y-m-d');
+            if (!is_null($calendar->getBadge())) $event['badge'] = $calendar->getBadge();
+            if (!is_null($calendar->getTitle())) $event['title'] = $calendar->getTitle();
+            if (!is_null($calendar->getClassname())) $event['classname'] = $calendar->getClassname();
+            $data[] = $event;
+        }
+        // dump($this->json($data));
+
         return $this->render('contributor/index.html.twig', [
-            'controller_name' => 'MainController',
+            'events' => $this->json($data),
         ]);
+
+
+        // $normalizer = new ObjectNormalizer();
+        // $serializer = new Serializer([$normalizer]);        
+        // $dataOut = $serializer->normalize($data);
+        // dump($data);
+
+        // return $this->render('contributor/index.html.twig', [
+        //     'events' => $dataOut,
+        // ]);
     }
 
     /**
